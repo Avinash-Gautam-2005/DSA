ddd06a7f79d9120898837887e43aa7408e472303
Avinash Kumar Gautam <avigautam2005@gmail.com>
2026-02-04 13:06:34 +0530

    Time: 2 ms (55.83%) | Memory: 7.6 MB (99.67%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isBadVersion(mid)) {
+                high = mid;        // go left
+            } else {
+                low = mid + 1;     // go right
+            }
+        }
+        return low;
+    }
+};
