0c4cd680ac676f7ec30b74c64b13dbb7ff2a5786
Avinash Kumar Gautam <avigautam2005@gmail.com>
2026-07-21 14:43:24 +0530

    Time: 4 ms (2.09%) | Memory: 11.4 MB (5.14%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    bool f(int i ,int j , string& s1 , string& s2){
+    bool f(int i ,int j , string& s1 , string& s2,vector<vector<int>>& dp){
         if(i<0) return true;
         if(j<0) return false;
+        if(dp[i][j]!=-1) return dp[i][j];
         bool pick = false;
         if(s1[i]== s2[j]){
-            pick = f(i-1,j-1,s1,s2);
+            pick = f(i-1,j-1,s1,s2,dp);
         }
-        bool notpick = f(i,j-1,s1,s2);
-        return pick || notpick;
+        bool notpick = f(i,j-1,s1,s2,dp);
+        return dp[i][j] = pick || notpick;
 
     }
     bool isSubsequence(string s, string t) {
         int n = s.size();
         int m = t.size();
-        return f(n-1,m-1,s,t);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,t,dp);
     }
 };
